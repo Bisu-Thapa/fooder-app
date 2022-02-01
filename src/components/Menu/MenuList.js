9c84dec9965331735dabeef6627941541bfb522e
Bisu-Thapa <b4bisu.thapa@gmail.com>
2022-02-01 22:17:34 +0200

    MenuList: removed hardcoded data, created database in firebase and with async function, fetched data from API

--- src/components/Menu/MenuList.js
+++ src/components/Menu/MenuList.js
@@ -1,50 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ItemsInfo from "./ItemsInfo";
 import "./Style.css";
 
-// This is a hardcoded dummy menu
-const Dummy_Menu = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Burritos",
-    description: "A mexican specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Pizza",
-    description: "cheese, ananas, chicken, salami",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Momo",
-    description: "Nepalese dumplings with spicy pickle",
-    price: 16.99,
-  },
-  {
-    id: "m5",
-    name: "Chicken soup bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const MenuList = () => {
+  //useState
+  const [menu, setMenu] = useState([]);
+  //useEffect: here we want our fetch only runs at the beginning
+  useEffect(() => {
+    // fetch: get data from API
+    const fetchMenu = async () => {
+      const response = await fetch(
+        "https://food-order-app-8c46e-default-rtdb.europe-west1.firebasedatabase.app/menu.json"
+      );
+      const responseData = await response.json();
+      const loadedMenu = [];
+      // for loop to go to all keys in responseData Object
+      for (const key in responseData) {
+        loadedMenu.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMenu(loadedMenu);
+    };
+    fetchMenu();
+  }, []);
+
   // used map()
-  const menuList = Dummy_Menu.map((menu) => (
+  const menuList = menu.map((item) => (
     <ItemsInfo
-      key={menu.id}
-      id={menu.id}
-      name={menu.name}
-      description={menu.description}
-      price={menu.price}
+      key={item.id}
+      id={item.id}
+      name={item.name}
+      description={item.description}
+      price={item.price}
     />
   ));
   return <ul>{menuList}</ul>;
